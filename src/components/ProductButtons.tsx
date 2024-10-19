73b454fbde33ec3efa316b6749718aed807cf581
misterdan100 <danielmca60@gmail.com>
2024-10-19 04:42:43 -0500

    first commit, build ok

---
+++ src/components/ProductButtons.tsx
@@ -0,0 +1,37 @@
+import React, { CSSProperties, useCallback, useContext } from "react"
+import { ProductContext } from "./ProductCard"
+import styles from '../styles/styles.module.css'
+
+export interface ProductButtonsProps {
+    className?: string
+    style?: CSSProperties
+}
+
+
+export const ProductButtons = ({ className, style }: ProductButtonsProps) => {
+    const { increaseBy, counter, maxCount } = useContext(ProductContext)
+
+    const isMaxReached = useCallback(() => {
+        if(maxCount) {
+            return counter >= maxCount
+        }
+        return false
+    }, [counter, maxCount])
+    
+    return (
+        <div className={`${styles.buttonsContainer} ${className}`} style={style}>
+                <button
+                    className={styles.buttonMinus}
+                    onClick={() => increaseBy(-1)}
+                >-</button>
+
+            <div className={styles.countLabel}> {counter} </div>
+
+                <button
+                    className={`${styles.buttonAdd} ${isMaxReached() && styles.disable}`}
+                    onClick={() => increaseBy(1)}
+                    // onClick={onChange}
+                >+</button>
+        </div>
+    )
+}
---
+++ src/components/ProductImage.tsx
@@ -0,0 +1,18 @@
+import React, { CSSProperties, useContext } from "react"
+import { ProductContext } from "./ProductCard"
+import noImage from '../assets/no-image.jpg'
+import styles from '../styles/styles.module.css'
+
+export interface ProductImageProps {
+    img?: string
+    className?: string
+    style?: CSSProperties
+}
+
+export const ProductImage = ({img = '', className, style}: ProductImageProps) => {
+    const { product } = useContext(ProductContext)
+    const image = img ? img : product.img ? product.img : noImage
+    return (
+        <img className={`${styles.productImg} ${className}`} src={image} alt="product image" style={style}/>
+    )
+}
